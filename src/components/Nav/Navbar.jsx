b2dfd5cfe126d8abdece1b57b85b39b6a54a0191
André Rodrigues <andrerodry23@gmail.com>
2024-04-24 18:56:31 -0300

    portfolio

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,6 +1,6 @@
 import logo from "../../assets/logo/logo.png"
 import andre from "../../assets/perfil/andre.jpg"
-import { useState } from 'react'
+import { useState } from "react"
 import CustomButton from "../CustomButton/CustomButton"
 import { Link } from "react-router-dom"
 import * as S from "./navbar_styled"
@@ -9,84 +9,72 @@ import { FaGithub } from "react-icons/fa"
 import { FaLinkedinIn } from "react-icons/fa6"
 
 export default function Navbar() {
+    const [menuvisivel, setMenuvisivel] = useState(false);
 
-  const [menuvisivel, setMenuvisivel] = useState(false);
+    const AbriMenu = () => {
+        setMenuvisivel(true);
+    };
 
-  const AbriMenu = () => {
-    setMenuvisivel(true);
-  };
+    const FechaMenu = () => {
+        setMenuvisivel(false);
+    };
 
-  const FechaMenu = () => {
-    setMenuvisivel(false);
-  };
+    const menuItemClicado = () => {
+        FechaMenu();
+    };
 
-  const menuItemClicado = ( ) => {
-    FechaMenu();
-  };
+    return (
+        <S.BoxMenu>
+            <S.BoxLogo>
+                <figure>
+                    <Link to="/">
+                        <img src={logo} alt="image logo" />
+                    </Link>
+                </figure>
 
-  return (
+                <button onClick={AbriMenu}>
+                    <TfiAlignCenter />
+                </button>
+            </S.BoxLogo>
 
-    <S.BoxMenu>
+            <nav className={`nav-container ${menuvisivel ? "exibir" : ""}`}>
+                <S.BoxList className={`menu ${menuvisivel ? "visivel" : ""}`}>
+                    <div onClick={FechaMenu}>
+                        <CustomButton text="Fechar" />
+                    </div>
 
-      
-      <S.BoxLogo>
-        <figure>
-        <Link to="/">
-        <img src={logo} alt="image logo"/>
-        </Link>
+                    <Link to="/">
+                        <img src={andre} alt="image logo" onClick={menuItemClicado} />
+                    </Link>
 
-        </figure>
-      
-      <button onClick={AbriMenu}>
-      <TfiAlignCenter />
-      </button>
+                    <h2>Andre Rodrigues</h2>
 
-      </S.BoxLogo>
+                    <S.MyUl>
+                        <li onClick={menuItemClicado} className="nav-item-one">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li onClick={menuItemClicado} className="nav-item-two">
+                            <Link to="/Sobre">Sobre</Link>
+                        </li>
+                        <li onClick={menuItemClicado} className="nav-item-three">
+                            <Link to="/Projetos">Projetos</Link>
+                        </li>
+                        <li onClick={menuItemClicado} className="nav-item-four">
+                            <Link to="/contato">Contato</Link>
+                        </li>
+                    </S.MyUl>
 
-    
+                    <S.IconBox>
+                        <S.Icon href="#">
+                            <FaLinkedinIn />
+                        </S.Icon>
 
-      <nav className={`nav-container ${menuvisivel ? 'exibir' : ''}`}>
-        
-
-        <S.BoxList className={`menu ${menuvisivel ? 'visivel' : ''}`}>
-
-              <div  onClick={FechaMenu}>
-              <CustomButton text="Fechar"/>
-              </div>
-   
-     
-        <Link to="/">
-        <img src={andre} alt="image logo" onClick={menuItemClicado}/>
-        </Link>
-     
-           <h2>Andre Rodrigues</h2>
-        
-          <S.MyUl>
-            <li onClick={menuItemClicado} className="nav-item-one"><Link to="/">Home</Link></li>
-            <li onClick={menuItemClicado} className="nav-item-two"><Link to="/Sobre">Sobre</Link></li>
-            <li onClick={menuItemClicado} className="nav-item-three"><Link to="/Projetos">Projetos</Link></li>
-            <li onClick={menuItemClicado} className="nav-item-four"><Link to="/contato">Contato</Link></li>
-          </S.MyUl>
-
-
-          <S.IconBox>
-            <S.Icon href="#">
-             <FaLinkedinIn />
-            </S.Icon>
-
-            <S.Icon href="#">
-              <FaGithub />
-            </S.Icon>
-
-          </S.IconBox>
-
-
-        </S.BoxList>
-
-
-      </nav>
-
-
-    </S.BoxMenu>
-  );
+                        <S.Icon href="#">
+                            <FaGithub />
+                        </S.Icon>
+                    </S.IconBox>
+                </S.BoxList>
+            </nav>
+        </S.BoxMenu>
+    );
 }
