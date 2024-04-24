a67e4434b38b43a8855ce8dd1b34842fc2f2bbd5
André Rodrigues <andrerodry23@gmail.com>
2024-04-24 20:08:04 -0300

    portfolio

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,27 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Sobre from "./pages/Sobre/Sobre";
-import Projetos from "./pages/Projetos/Projetos";
-import Contato from "./pages/Contato/Contato";
-import Header from "./components/Header/Header";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Sobre from "./pages/Sobre/Sobre"
+import Projetos from "./pages/Projetos/Projetos"
+import Contato from "./pages/Contato/Contato"
+import Header from "./components/Header/Header"
 
 export default function Rotas() {
-  return (
-    <div>
-     
-      <BrowserRouter>
-      <Header/>
-      <Routes>
-
-       <Route path="/" element={<Home/>}/>
-       <Route path="sobre" element={<Sobre/>}/>
-       <Route path="projetos" element={<Projetos/>}/>
-       <Route path="contato" element={<Contato/>}/>
-
-      </Routes>
-     
-      </BrowserRouter>
-    </div>
-  )
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="sobre" element={<Sobre />} />
+                    <Route path="projetos" element={<Projetos />} />
+                    <Route path="contato" element={<Contato />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
