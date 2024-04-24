54287f2fccd04c67dbf57f59bdd501b208904b53
André Rodrigues <andrerodry23@gmail.com>
2024-04-24 20:12:32 -0300

    portfolio

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,48 +1,38 @@
-import * as S from "./sobre_styled"
-import seta1 from "../../assets/setascode/seta1.png"
-import seta2 from "../../assets/setascode/seta2.png"
-import andre from "../../assets/perfil/andre.jpg"
+import * as S from "./sobre_styled";
+import seta1 from "../../assets/setascode/seta1.png";
+import seta2 from "../../assets/setascode/seta2.png";
+import andre from "../../assets/perfil/andre.jpg";
 
 export default function Sobre() {
-  return (
-    <S.Main>
-      <section>
-        <S.Title>
-          
-          <h2>Sobre</h2>
+    return (
+        <S.Main>
+            <section>
+                <S.Title>
+                    <h2>Sobre</h2>
+                </S.Title>
 
+                <S.Container>
+                    <figure>
+                        <img src={andre} alt="" />
+                    </figure>
 
-        </S.Title>
-        
-        <S.Container>
-          <figure>
-            <img src={andre} alt="" />
-          </figure>
-          
-          <S.Journey>
-            <S.Profile>
-              <h3>André Rodrigues</h3>
-              <img src={seta1} alt="" className="seta1" />
-              <img src={seta2} alt="" className="seta2" />
-            </S.Profile>
-            
-            <p>
-              Minha jornada profissional teve início em 2023, quando decidi ingressar na 
-              faculdade para cursar Análise e Desenvolvimento de Sistemas. Paralelamente ao 
-              curso, mergulhei no mundo do desenvolvimento full stack. No meio de tantas 
-              atividades, descobri minha paixão pelo desenvolvimento de sites.
-              Como desenvolvedor front-end, tenho conhecimento nas tecnologias essenciais como HTML, CSS, 
-              JavaScript, React, entre outras. Participei de projetos desafiadores, nos 
-              quais pude não apenas aprimorar minhas habilidades técnicas, mas também desenvolver 
-              competências interpessoais, como responsabilidade, proatividade e trabalho em equipe.
-              Acredito firmemente que o sucesso de uma equipe de desenvolvimento vai além das habilidades 
-              técnicas individuais. Envolve, também, colaboração e aprendizado contínuo. Por isso, 
-              mantenho-me sempre atualizado com as últimas tendências do mercado e busco constantemente 
-              oportunidades de aprimoramento e crescimento profissional.
-            </p>
-          </S.Journey>
-        </S.Container>
-      </section>
-    </S.Main>
-  );
+                    <S.Journey>
+                        <S.Profile>
+                            <h3>André Rodrigues</h3>
+                            <img src={seta1} alt="" className="seta1" />
+                            <img src={seta2} alt="" className="seta2" />
+                        </S.Profile>
+
+                        <p>
+                            Minha jornada profissional teve início em 2023, quando decidi ingressar na faculdade para cursar Análise e Desenvolvimento de Sistemas. Paralelamente ao curso, mergulhei no mundo do desenvolvimento full stack. No
+                            meio de tantas atividades, descobri minha paixão pelo desenvolvimento de sites. Como desenvolvedor front-end, tenho conhecimento nas tecnologias essenciais como HTML, CSS, JavaScript, React, entre outras.
+                            Participei de projetos desafiadores, nos quais pude não apenas aprimorar minhas habilidades técnicas, mas também desenvolver competências interpessoais, como responsabilidade, proatividade e trabalho em equipe.
+                            Acredito firmemente que o sucesso de uma equipe de desenvolvimento vai além das habilidades técnicas individuais. Envolve, também, colaboração e aprendizado contínuo. Por isso, mantenho-me sempre atualizado com
+                            as últimas tendências do mercado e busco constantemente oportunidades de aprimoramento e crescimento profissional.
+                        </p>
+                    </S.Journey>
+                </S.Container>
+            </section>
+        </S.Main>
+    );
 }
--- src/pages/Sobre/sobre_styled.jsx
+++ src/pages/Sobre/sobre_styled.jsx
@@ -1,35 +1,33 @@
-import styled, {css } from "styled-components"
+import styled, { css } from "styled-components"
 import SobreBack from "../../assets/background/SobreBack.gif"
 import backSobre from "../../assets/background/backSobre.png"
 import { maxWidths } from "../../components/MaxWidths/maxWidths"
 
-
 const center = css`
-display: flex;
-align-items: center;
-justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const Main = styled.main`
-  width: 100%;
-  height: auto;
-  /* border: 4px solid #FF76CE; */
- `
-
+    width: 100%;
+    height: auto;
+    /* border: 4px solid #FF76CE; */
+`;
 
 export const Title = styled.div`
-  ${center}
-  height: 100px;
-  background-image: url(${SobreBack});
-  background-position: 40% 10%;
+    ${center}
+    height: 100px;
+    background-image: url(${SobreBack});
+    background-position: 40% 10%;
 
-  h2 {
-    font-size: 2rem;
-    color: #F6F5F2;
-    font-weight: 900;
-    text-transform: capitalize;
-  }
-`
+    h2 {
+        font-size: 2rem;
+        color: #f6f5f2;
+        font-weight: 900;
+        text-transform: capitalize;
+    }
+`;
 
 export const Container = styled.div`
     ${center}
@@ -41,134 +39,123 @@ export const Container = styled.div`
     /* border: 4px solid #80B3FF; */
 
     figure {
-      width: 50%;
-       /* border: 3px solid #E72929;  */
-    @media (min-width: 340px) {
-      width: 135px;
-  }
+        width: 50%;
+        /* border: 3px solid #E72929;  */
+        @media (min-width: 340px) {
+            width: 135px;
+        }
     }
 
     img {
-      width: 100%;
+        width: 100%;
     }
-`
+`;
 
 export const Journey = styled.div`
- width: 100%;
- background-image: url(${backSobre});
- background-position: 20% 100%;
- background-repeat: no-repeat;
- background-size: cover;;
- /* border: 4px solid #3C0753;  */
+    width: 100%;
+    background-image: url(${backSobre});
+    background-position: 20% 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    /* border: 4px solid #3C0753;  */
 
- @media (min-width: 340px) {
-  max-width: ${maxWidths.small};
-  }
-  
-  @media (min-width: 550px) {
-  max-width: ${maxWidths.medium};
-  }
+    @media (min-width: 340px) {
+        max-width: ${maxWidths.small};
+    }
 
-  @media (min-width: 768px) {
-  max-width: ${maxWidths.large};
-  }
+    @media (min-width: 550px) {
+        max-width: ${maxWidths.medium};
+    }
 
+    @media (min-width: 768px) {
+        max-width: ${maxWidths.large};
+    }
 
+    p {
+        text-align: start;
+        word-spacing: 2px;
+        font-weight: 600;
+        letter-spacing: 2px;
+        line-height: 21px;
+        color: #f6f5f2;
+        font-size: clamp(2px, 5vw, 13px);
 
-p{ 
-  text-align: start;
-  word-spacing: 2px;
-  font-weight: 600;
-  letter-spacing: 2px;
-  line-height: 21px;
-  color: #F6F5F2;
-  font-size: clamp(2px, 5vw, 13px);
+        @media (min-width: 340px) {
+            line-height: 24px;
+            font-size: clamp(2px, 4vw, 15px);
+        }
 
-  @media (min-width: 340px) {
-    line-height: 24px;
-    font-size: clamp(2px, 4vw, 15px);
-  }
+        @media (min-width: 550px) {
+            line-height: 29px;
+            font-size: clamp(2px, 4vw, 15px);
+        }
 
-  @media (min-width: 550px) {
-    line-height: 29px;
-    font-size: clamp(2px, 4vw, 15px);
-  }
+        @media (min-width: 768px) {
+            line-height: 33px;
+            font-size: clamp(2px, 5vw, 16px);
+        }
 
-  @media (min-width: 768px) {
-    line-height: 33px;
-    font-size: clamp(2px, 5vw, 16px);
-  }
+        @media (min-width: 1200px) {
+            text-align: center;
+            word-spacing: 4px;
+            letter-spacing: 3.5px;
+            font-size: clamp(2px, 4vw, 15px);
+        }
 
-  @media (min-width: 1200px) {
-    text-align: center;
-    word-spacing: 4px;
-    letter-spacing: 3.5px;
-    font-size: clamp(2px, 4vw, 15px);
-    
-  }
-
-
-  @media (max-width: 340px) {
-    font-size: clamp(1px, 3.8vw, 20px);
-  }
-
-  
-}
-`
+        @media (max-width: 340px) {
+            font-size: clamp(1px, 3.8vw, 20px);
+        }
+    }
+`;
 
 export const Profile = styled.div`
- ${center}
- width: 100%;
- height: 80px;
- margin-inline: auto;
- position: relative;
- transition: width 0.5s ease;
- /* border: 4px solid #EBF400;  */
-
-@media (min-width: 340px) {
-    width: 280px;
-    margin-bottom: 20px;
-  }
-
-  @media (min-width: 550px) {
-    width: 360px;
-  }
-
-
-  @media (max-width: 340px) {
+    ${center}
     width: 100%;
+    height: 80px;
+    margin-inline: auto;
+    position: relative;
+    transition: width 0.5s ease;
+    /* border: 4px solid #EBF400;  */
 
-  }
+    @media (min-width: 340px) {
+        width: 280px;
+        margin-bottom: 20px;
+    }
 
+    @media (min-width: 550px) {
+        width: 360px;
+    }
 
- h3 {
-  font-size: clamp(2px, 6vw, 17px);
-  color:#F6F5F2;
+    @media (max-width: 340px) {
+        width: 100%;
+    }
 
-  @media (min-width: 340px) {
-    font-size: clamp(2px, 5vw, 20px);
-  }
+    h3 {
+        font-size: clamp(2px, 6vw, 17px);
+        color: #f6f5f2;
 
-  @media (max-width: 340px) {
-    font-size: clamp(1px, 5vw, 20px);
-  }
+        @media (min-width: 340px) {
+            font-size: clamp(2px, 5vw, 20px);
+        }
 
-  @media (min-width: 550px) {
-    font-size: clamp(2px, 4vw, 24px);
-  }
-  
- }
+        @media (max-width: 340px) {
+            font-size: clamp(1px, 5vw, 20px);
+        }
 
- .seta2 {
-  width: 16%;
-  right: 0;
-  position: absolute;
- }
+        @media (min-width: 550px) {
+            font-size: clamp(2px, 4vw, 24px);
+        }
+    }
 
- .seta1 {
-  width: 15.8%;
-  left: 0;
-  position: absolute;
- }
+    .seta2 {
+        width: 16%;
+        right: 0;
+        position: absolute;
+    }
 
-`
+    .seta1 {
+        width: 15.8%;
+        left: 0;
+        position: absolute;
+    }
+`;
