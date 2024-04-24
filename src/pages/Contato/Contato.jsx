38873686a1e1edca67e11fe4e2e6525830fda8c3
André Rodrigues <andrerodry23@gmail.com>
2024-04-24 19:11:16 -0300

    portfolio

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,78 +1,76 @@
 import * as S from "./contato_styled"
 import { AiOutlineMail } from "react-icons/ai"
-import { FaWhatsapp } from "react-icons/fa6"
+import { FaWhatsapp } from "react-icons/fa"
 
 export default function Contato() {
   return (
-    
     <S.Main>
+      <S.Title>Contato</S.Title>
+      <S.MySection>
+        <S.Form
+          action="mailto:andrelopesr2018@gmail.com"
+          method="post"
+          encType="text/plain" // Corrigido para encType
+        >
+          <label htmlFor="name"></label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Seu nome..."
+            required
+          />
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="e-mail@email.com"
+            required
+          />
+          <label htmlFor="subject"></label>
+          <input
+            type="text"
+            name="subject"
+            id="subject"
+            placeholder="Qual o assunto da mensagem?"
+            required
+          />
+          <label htmlFor="message"></label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Mensagem"
+            required
+          ></textarea>
+          <button type="submit">Enviar mensagem</button>
+        </S.Form>
 
-        <S.Title>contato</S.Title>
-      
-    <S.MySection>
-
-      
-      <S.Form
-        action="mailto:andrelopesr2018@gmail.com"
-        method="post"
-        enctype="text/plain"
-      >
-        <label htmlFor="name"></label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Seu nome..."
-          required
-        />
-        
-        <label htmlFor="email"></label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="e-mail@email.com"
-          required
-        />
-        
-        <label htmlFor="subject"></label>
-        <input
-          type="text"
-          name="subject"
-          id="subject"
-          placeholder="Qual o assunto da mensagem?"
-          required
-        />
-        
-        <label htmlFor="message"></label>
-        <textarea
-          name="message"
-          id="message"
-          placeholder="Mensagem"
-          required
-        ></textarea>
-        
-        <button type="submit">Enviar mensagem</button>
-      </S.Form>
-
-      <S.Contact>
-
-        
-      <a href="#" target="_blank" className="contact-link">
-        <span className="icon-whats">
-        <FaWhatsapp />
-        </span>
-        <span className="name">Andre Rodrigues</span>
-        </a>
-
-      <a href="mailto:andrelopesr2018@gmail.com" target="_blank" className="email-link">
-      <span className="icon-email">
-      <AiOutlineMail />
-       </span>
-      <span className="email-address">andrelopesr2018@gmail.com</span>
-      </a>
-      </S.Contact>
-    </S.MySection>
-  </S.Main>  
+        <S.Contact>
+          <a
+            href="https://api.whatsapp.com/send?phone=SEUNUMERODETELEFONE"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact-link"
+          >
+            <span className="icon-whats">
+              <FaWhatsapp />
+            </span>
+            <span className="name">Andre Rodrigues</span>
+          </a>
+          <a
+            href="mailto:andrelopesr2018@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="email-link"
+          >
+            <span className="icon-email">
+              <AiOutlineMail />
+            </span>
+            <span className="email-address">andrelopesr2018@gmail.com</span>
+          </a>
+        </S.Contact>
+      </S.MySection>
+    </S.Main>
   );
 }
