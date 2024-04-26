ed34e97b0bf4cb5e2bdb15d898b74adbc3de6393
André Rodrigues <andrerodry23@gmail.com>
2024-04-26 10:45:03 -0300

    portfolio

--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -2,7 +2,7 @@ import { Carousel } from 'react-responsive-carousel'
 import * as S from "./projetos_styled"
 import projeto1 from "../../assets/projetos/projeto1.gif"
 import reyNegro from "../../assets/projetos/reyNegro.gif"
-//import selfCare from "../../assets/projetos/selfCare.gif"
+import selfCare from "../../assets/projetos/selfCare.gif"
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 export default function Projetos() {
@@ -11,27 +11,27 @@ export default function Projetos() {
       imagem: projeto1,
       titulo: "projeto 1",
       descricao: "Projeto criado criado com o intuito de mostrar meus conhecimentos e projetos práticos. Ele é totalmente responsivo podendo ser acessado por um celular ou tablet. Foi criado utilizando as seguintes tecnologias:",
-      altTexto: "Alt texto do primeiro projeto",
-      websiteLink: "https://seuwebsite1.com",
-      githubLink: "https://seugithub1.com",
+      altTexto: "",
+      websiteLink: "",
+      githubLink: "",
       tecnologias: ["React", "Styled-components", "Vite", "Git"],
     },
     {
       imagem: reyNegro,
       titulo: "projeto 2",
       descricao: "Projeto criado com o intuito de mostrar meus conhecimentos e projetos práticos. Ele é totalmente responsivo podendo ser acessado por um celular ou tablet. Foi criado utilizando as seguintes tecnologias:",
-      altTexto: "Alt texto do segundo projeto",
-      websiteLink: "https://seuwebsite2.com",
-      githubLink: "https://seugithub2.com",
+      altTexto: "",
+      websiteLink: "",
+      githubLink: "",
       tecnologias: ["React", "Styled-components", "Vite", "Git"],
     },
     {
-      imagem: reyNegro,
+      imagem: selfCare,
       titulo: "projeto 3",
       descricao: "Projeto criado com o intuito de mostrar meus conhecimentos e projetos práticos. Ele é totalmente responsivo podendo ser acessado por um celular ou tablet. Foi criado utilizando as seguintes tecnologias:",
-      altTexto: "Alt texto do terceiro projeto",
-      websiteLink: "https://seuwebsite3.com",
-      githubLink: "https://seugithub3.com",
+      altTexto: "",
+      websiteLink: "",
+      githubLink: "",
       tecnologias: ["React", "Styled-components", "Vite", "Git"],
     },
   ];
